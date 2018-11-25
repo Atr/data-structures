897f3d99e9a208402db20fe38093560719843352
Alex Romanak <alexromanak@Alexs-MacBook-Pro.local>
2018-11-24 22:53:58 -0800

    Added spacing to hashTableHelpers.js

--- hash-table/hashTableHelpers.js
+++ hash-table/hashTableHelpers.js
@@ -9,14 +9,17 @@ const LimitedArray = function(limit) {
   const storage = [];
 
   const limitedArray = {};
+
   limitedArray.get = function(index) {
     checkLimit(index);
     return storage[index];
   };
+
   limitedArray.set = function(index, value) {
     checkLimit(index);
     storage[index] = value;
   };
+  
   limitedArray.each = function(callback) {
     for (let i = 0; i < storage.length; i++) {
       callback(storage[i], i, storage);
